9f1d8fa426749c1d2758ffde25db83a237363c86
Ayoub <elpasha117@gmail.com>
2023-02-21 22:47:41 +0100

    update

--- src/scenes/Register/index.js
+++ src/scenes/Register/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles} from '@material-ui/core/styles';
 import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
@@ -8,176 +8,150 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 
-function ShowFailedAlert () 
-{
-  return (
-    <Alert variant = "outlined" color = "error" severity="error"> Please complete all the fields before registering</Alert>
-  );
-}
-function ShowSuccessAlert () 
-{
-  return (
-    <Alert variant = "inlined" color = "success" severity="success"> Account created ! Return to the home page to sign in</Alert>
-  );
-}
+
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      },
-      body:
-      {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background:'linear-gradient(0deg, rgba(62,64,75,1) 0%, rgba(62,64,75,1) 75%, rgba(40,43,54,1) 100%)', 
-      },
-      paper: 
-      {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      form: 
-      {
-        width: '100%',
-        marginTop: theme.spacing(1),
-      },
-      submit: {
-        margin: theme.spacing(3, 0, 2),
-        backgroundColor: '#4c9aff',
-        color: 'white',
-      },
-      textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: '100%',
-      },
+    container: 
+    {
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    body:
+    {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background:'linear-gradient(0deg, rgba(62,64,75,1) 0%, rgba(62,64,75,1) 75%, rgba(40,43,54,1) 100%)'
+    },
+    paper: 
+    {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    form: 
+    {
+      width: '100%',
+      marginTop: theme.spacing(1),
+    },
+    submit: 
+    {
+      margin: theme.spacing(3, 0, 2),
+      backgroundColor: '#4c9aff',
+      color: 'white',
+    },
+    textField: {
+      fontColor: 'white',
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: '100%',
+    },
 }));
 
-export default function SignUpForm() {
-  const classes = useStyles();
-  const navigate = useNavigate();
-  const [username, setUsername] = useState();
-  const [secret, setSecret] = useState();
-  const [first_name, setFirstName] = useState();
-  const [last_name, setLastName] = useState();
-  const [isError, setError] = useState(false);
-  const [isSuccess, setSuccess] = useState(false);
-  const [age, setAge] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    const email = username;
-    const password = secret;
+export default function emailForm() 
+{
+    const classes = useStyles();
+    const [email, setEmail] = useState('');
+    const [isError, setError] = useState(false);
+    const [isReg, setReg] = useState(false);
+    const [isUmpEmail, setUmpEmail] = useState(true);
+    const [first_name, setFirstName] = useState('');
+    const [last_name, setLastName] = useState('');
 
-    axios.post("http://localhost:8080/v1/auth/register", { email, password, age })
-      .then(response => 
+    function ShowSuccessAlert () 
+    {
+      return (
+        <Alert variant = "filled" color = "success" severity="success"> Welcome {first_name} {last_name} </Alert>
+      );
+    }
+
+
+    function ShowEmailAlert () 
+    {
+      return (
+        <Alert variant = "filled" color = "warning" severity="warning"> Please use an valid @ump.ac.ma email !</Alert>
+      );
+    }
+   
+
+    const handleSubmit = (e) => 
+    {
+      e.preventDefault();
+      if(email.endsWith("@ump.ac.ma"))
       {
-      console.log(response.data);
-      if(response.status === 200)
-      {
-        setSuccess(true);
-      }
-      else setError(true); 
-       
+        setUmpEmail(true);
+        axios.post("http://localhost:8080/v1/auth/email", { email })
+        .then(response => 
+        {
+        if (response.status === 200) 
+        {
+          console.log(response.data);
+          setFirstName(response.data.firstName);
+          setLastName(response.data.lastName);
+          setError(false);
+          setReg(true);
+        }
+
       })
       .catch(error => 
-      {
+      { 
+        setReg(false);
         setError(true);
-        console.log(error);
       });
-    axios
-    .post("http://localhost:8080/v1/auth/groupchat/register", {
-      username,
-      secret,
-      email,
-      first_name,
-      last_name,
-      })
-      .then((r) => e({ ...r.data, secret })) // NOTE: over-ride secret
-      .catch(error =>{
-        console.log(error);
-      });
-  }
+    }
+    else
+    {
+      setReg(false);
+      setUmpEmail(false);
+    }
+    }
 
-  return (
-    <div className={classes.body}>
-      <Container className={classes.container}>
-        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
-          <Grid container spacing={4}>
-            <Grid item xs={10} sm={6}>
-              <TextField
-                required
-                id="firstName"
-                label="First Name"
-                fullWidth
-                variant="outlined"
-                value={first_name}
-                onChange={e => setFirstName(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="lastName"
-                label="Last Name"
-                fullWidth
-                variant="outlined"
-                value={last_name}
-                onChange={e => setLastName(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="Email Adress"
-                label="Email Adress"
-                fullWidth
-                variant="outlined"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="password"
-                label="Password"
-                fullWidth
-                variant="outlined"
-                value={secret}
-                onChange={e => setSecret(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                id="apogeeNumber"
-                label="Apogee Number"
-                fullWidth
-                variant="outlined"
-                value={age}
-                onChange={e => setAge(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button variant="contained" color="primary" type="submit">
-                Sign in
-              </Button>
-            </Grid>
+    return (
+        <div className={classes.body} >
+          <Container maxWidth="xs"> 
+          <div className={classes.paper} style={{ padding: 20 }}>            
+          <Grid container className={classes.container}>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <TextField
+                     className = "textfield"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+              <Grid container justify="center" alignItems="center">
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  className={classes.submit}
+                >
+                  Sign up
+                </Button>
+              </Grid>
+            <div>
+              {isReg ? (<ShowSuccessAlert/>) : (null)}
+            </div>
+            <div>
+              {isUmpEmail ? (null) : (<ShowEmailAlert/>)}
+            </div>
+            </form>
           </Grid>
-          <div>
-              {isError ? (<ShowFailedAlert/>) : (null)}
-              {isSuccess ? (<ShowSuccessAlert/>) : (null)}
           </div>
-        </form>
-      </Container>
-    </div>
-  );
+        </Container>
+        </div>
+    );
 }
